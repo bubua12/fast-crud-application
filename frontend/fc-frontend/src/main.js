e1759416acf9d7a9ce27ddc4242e87437ad6f030
wangxl <wangty334752@gmail.com>
2026-06-11 15:21:49 +0800

    feat: 删除模板文件

--- frontend/fc-frontend/src/main.js
+++ frontend/fc-frontend/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import App from './App.vue' // 导入根组件
 
-createApp(App).mount('#app')
+app  = createApp(App) // 创建应用实例，传入根组件
+app.mount('#app') // 挂载到 index.html 中 id="app" 的 DOM 元素上
